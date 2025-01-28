e2e44badc6000fb1ca124752062472f268bfe5f7
matheus <matheus.pereira@zup.com.br>
2025-01-28 18:35:05 -0300

    feat: adiciona a model Client

---
+++ src/main/java/com/zup/ecommerce/models/Client.java
@@ -0,0 +1,10 @@
+package com.zup.ecommerce.models;
+
+public class Client {
+    private String name;
+    private String CPF;
+    private String email;
+    /*
+        if (client.email.exists() || cliente.cpf.exists() ) throw new argumentExistsException
+     */
+}
