888f694b51a8d2bdc19758e2c13fd801e6797126
matheus <matheus.pereira@zup.com.br>
2025-01-28 18:35:15 -0300

    feat: adiciona a model Product

---
+++ src/main/java/com/zup/ecommerce/models/Product.java
@@ -0,0 +1,11 @@
+package com.zup.ecommerce.models;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    /*
+    validação:
+    if(name.exists) thow new ArgumentExistisException
+     */
+}
