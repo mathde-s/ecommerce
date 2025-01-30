25165e87ec8b530c0d2c62701e8fc456f87dd5cc
matheus <matheus.pereira@zup.com.br>
2025-01-30 20:34:57 -0300

    feat: adiciona o DTO clienteRequest

---
+++ src/main/java/com/zup/ecommerce/DTOs/ClientRequestDTO.java
@@ -0,0 +1,51 @@
+package com.zup.ecommerce.DTOs;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+public class ClientRequestDTO {
+    @NotNull(message = "o nome não pode ser nulo")
+    private String name;
+
+    @CPF(message = "o CPF deve ser valido")
+    @Column(unique = true, name = "email")
+    private String cpf;
+
+    @Email(message = "o email deve ser válido")
+    @Column(unique = true, name = "email")
+    private String email;
+
+    public ClientRequestDTO() {}
+
+    public ClientRequestDTO(String name, String cpf, String email) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String CPF) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
