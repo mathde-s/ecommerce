3dfc4001029f81e467b31a00419e66ced5cdf240
matheus <matheus.pereira@zup.com.br>
2025-01-30 20:35:21 -0300

    feat: adiciona o DTO clienteResponse

---
+++ src/main/java/com/zup/ecommerce/DTOs/ClientResponseDTO.java
@@ -0,0 +1,40 @@
+package com.zup.ecommerce.DTOs;
+
+public class ClientResponseDTO {
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+
+    public ClientResponseDTO(){}
+
+    public ClientResponseDTO(String name, String cpf, String email) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
