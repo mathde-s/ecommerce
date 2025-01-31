ce181e7c94866f130ed8e4c0d9969f013c5a8430
matheus <matheus.pereira@zup.com.br>
2025-01-31 10:59:25 -0300

    feat: get e set do id de ProductResponseDTO

--- src/main/java/com/zup/ecommerce/DTOs/ProductResponseDTO.java
+++ src/main/java/com/zup/ecommerce/DTOs/ProductResponseDTO.java
@@ -38,4 +38,12 @@ public class ProductResponseDTO {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
