2df3c881e503bd7cd8322289b8a264009fd58e52
matheus <matheus.pereira@zup.com.br>
2025-01-31 10:35:43 -0300

    feat: adiciona os repositories de product e client

---
+++ src/main/java/com/zup/ecommerce/repository/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.zup.ecommerce.repository;
+
+import com.zup.ecommerce.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Client findByCpf(String cpd);
+    boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
+
+}
