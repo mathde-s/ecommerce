9d5ae3d5a537f765c5b98d11585ed3a25e2a8c8c
matheus <matheus.pereira@zup.com.br>
2025-01-31 10:33:22 -0300

    feat: adiciona o controller de client

---
+++ src/main/java/com/zup/ecommerce/controller/ClientController.java
@@ -0,0 +1,32 @@
+package com.zup.ecommerce.controller;
+
+import com.zup.ecommerce.DTOs.ClientRequestDTO;
+import com.zup.ecommerce.DTOs.ClientResponseDTO;
+import com.zup.ecommerce.service.ClientService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/client")
+@Validated
+public class ClientController {
+
+    @Autowired
+    private ClientService clienteService;
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> createClient(@Valid @RequestBody ClientRequestDTO client) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.createClient(client));
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<ClientResponseDTO> getClientByCPF(@PathVariable @CPF(message = "CPF inválido") String cpf) {
+        return ResponseEntity.ok(clienteService.getClientByCpf(cpf));
+    }
+}
