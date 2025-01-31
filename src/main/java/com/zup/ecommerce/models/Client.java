e9af07e65c8d53aef60e469c96bccbadd8b2b3c8
matheus <matheus.pereira@zup.com.br>
2025-01-31 10:36:44 -0300

    refactor: adiciona validações de entrada, gets e setters

--- src/main/java/com/zup/ecommerce/models/Client.java
+++ src/main/java/com/zup/ecommerce/models/Client.java
@@ -1,10 +1,51 @@
 package com.zup.ecommerce.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
-    private String CPF;
+    private String cpf;
     private String email;
     /*
         if (client.email.exists() || cliente.cpf.exists() ) throw new argumentExistsException
      */
+    public Client() {}
+
+
+    public Client(String name, String cpf, String email) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
