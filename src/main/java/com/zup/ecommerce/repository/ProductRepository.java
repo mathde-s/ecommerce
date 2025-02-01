7af6bba9ae2069a917079024a286e22dc7937f3d
matheus <matheus.pereira@zup.com.br>
2025-02-01 02:41:34 -0300

    feat: adiciona o método que apaga pelo nome

--- src/main/java/com/zup/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/zup/ecommerce/repository/ProductRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     boolean existsByName(String name);
+    void deleteByName(String name);
 }
