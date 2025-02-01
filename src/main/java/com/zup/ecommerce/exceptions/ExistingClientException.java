64fc961768ce7eafa51bccc51328d57dcb1a8c0b
matheus <matheus.pereira@zup.com.br>
2025-02-01 02:09:13 -0300

    refactor: renomeia para generalizar outras entidades existentes

--- src/main/java/com/zup/ecommerce/exceptions/ExistingClientException.java
+++
@@ -1,8 +0,0 @@
-package com.zup.ecommerce.exceptions;
-
-
-public class ExistingClientException extends IllegalArgumentException{
-    public ExistingClientException(String message){
-        super(message);
-    }
-}
