7d629aa29444b7b240ce72df8bf457108359ca21
matheus <matheus.pereira@zup.com.br>
2025-02-01 03:05:16 -0300

    feat: adiciona os gets e setters de id, e construtores na classe ProductRequestDTO

--- src/main/java/com/zup/ecommerce/DTOs/ProductRequestDTO.java
+++ src/main/java/com/zup/ecommerce/DTOs/ProductRequestDTO.java
@@ -14,4 +14,38 @@ public class ProductRequestDTO {
 
     @Positive(message = "a quantidade não pode ser meno que zero")
     private int quantity;
+
+
+    public ProductRequestDTO() {
+    }
+
+    public ProductRequestDTO(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
