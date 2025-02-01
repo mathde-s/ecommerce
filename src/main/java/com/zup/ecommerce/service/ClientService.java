2f5ec03d7792009fa863a8b07a4b59a14211edcf
matheus <matheus.pereira@zup.com.br>
2025-02-01 01:47:49 -0300

    feat: adiciona validação de cpf vazio

--- src/main/java/com/zup/ecommerce/service/ClientService.java
+++ src/main/java/com/zup/ecommerce/service/ClientService.java
@@ -3,6 +3,7 @@ package com.zup.ecommerce.service;
 import com.zup.ecommerce.DTOs.ClientRequestDTO;
 import com.zup.ecommerce.DTOs.ClientResponseDTO;
 import com.zup.ecommerce.exceptions.ExistingClientException;
+import com.zup.ecommerce.exceptions.NullArgumentException;
 import com.zup.ecommerce.models.Client;
 import com.zup.ecommerce.repository.ClientRepository;
 import com.zup.ecommerce.utills.ClientMapper;
@@ -19,7 +20,7 @@ public class ClientService {
         Client client = ClientMapper.toEntity(clientRequestDTO);
 
         if(clientRepository.existsByCpf(client.getCpf()))
-            throw new ExistingClientException("o CPF:"+client.getCpf()+" já está cadastrado a um cliente");
+            throw new ExistingClientException("o CPF: "+client.getCpf()+" já está cadastrado a um cliente");
         if(clientRepository.existsByEmail(client.getEmail()))
             throw new ExistingClientException("o email: "+client.getEmail()+" já esta cadastrado a um cliente");
 
@@ -28,11 +29,15 @@ public class ClientService {
     }
 
     public ClientResponseDTO getClientByCpf(String cpf) {
+        if (cpf.isBlank())
+            throw new NullArgumentException("o cpf não pode vir vazio");
         Client client = clientRepository.findByCpf(cpf);
         return ClientMapper.toResponseDTO(client);
     }
 
     public ClientResponseDTO updateClient(String cpf, ClientRequestDTO requestDTO){
+        if (cpf.isBlank())
+            throw new NullArgumentException("o cpf não pode vir vazio");
         Client clientExists = clientRepository.findByCpf(cpf);
         Client clientSaved = clientRepository.save(clientExists);
         return ClientMapper.toResponseDTO(clientSaved);
