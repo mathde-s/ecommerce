321d4269e95961f732f7f86d0cca275d8c96d835
matheus <matheus.pereira@zup.com.br>
2025-02-04 18:10:24 -0300

    feat: adiciona a entidade Buy

---
+++ src/main/java/com/zup/ecommerce/models/Buy.java
@@ -0,0 +1,62 @@
+package com.zup.ecommerce.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Buy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Client client;
+
+    @ManyToMany
+    private List<Product> products;
+
+    private LocalDateTime BuyTime;
+
+    public Buy() {}
+
+    public Buy(Long id, Client client, List<Product> products) {
+        this.id = id;
+        this.client = client;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public LocalDateTime getBuyTime() {
+        return BuyTime;
+    }
+
+    public void setBuyTime(LocalDateTime buyTime) {
+        BuyTime = buyTime;
+    }
+}
