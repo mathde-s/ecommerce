f084e00f37b9f264e6579a5e2a29c376580d8202
matheus <matheus.pereira@zup.com.br>
2025-02-04 18:10:53 -0300

    feat: adiciona a interface de BuyRepository

---
+++ src/main/java/com/zup/ecommerce/repository/BuyRepository.java
@@ -0,0 +1,7 @@
+package com.zup.ecommerce.repository;
+
+import com.zup.ecommerce.models.Buy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuyRepository extends JpaRepository<Buy, Long> {
+}
